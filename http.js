07951ef81b2de3a9b97343e3e2a84410acc49823
Nubo Developer <dev@nubosoftware.com>
2016-05-30 15:31:48 +0300

    merge http.js with http.js from platform_server.js
    Change-Id: I2dd232ead8730c310383fd8f8023014ca0bc1400

--- http.js
+++ http.js
@@ -1,9 +1,79 @@
 "use strict";
 
+var http = require('http');
+var https = require('https');
+
 module.exports = {
+    doPostRequest: doPostRequest,
+    doGetRequest: doGetRequest,
     getDataFromRequest: getDataFromRequest,
     getObjFromRequest: getObjFromRequest
 };
+
+function doPostRequest(options, postData, callback) {
+    var callbackDone = false;
+    var resData = "";
+    var request;
+    if(options.key) request = https.request;
+    else request = http.request;
+    var req = request(
+        options,
+        function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                resData = resData + chunk;
+            });
+            res.on('end', function() {
+                if(!callbackDone) {
+                    callbackDone = true;
+                    callback(null, resData);
+                }
+            });
+        }
+    );
+
+    req.on('error', function(e) {
+        if(!callbackDone) {
+            callbackDone = true;
+            callback("Error while request", e);
+        }
+    });
+
+    req.write(postData);
+    req.end();
+}
+
+function doGetRequest(options, callback) {
+    var callbackDone = false;
+    var resData = "";
+    var request;
+    if(options.key) request = https.request;
+    else request = http.request;
+    var req = request(
+        options,
+        function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                resData = resData + chunk;
+            });
+            res.on('end', function() {
+                if(!callbackDone) {
+                    callbackDone = true;
+                    callback(null, resData);
+                }
+            });
+        }
+    );
+
+    req.on('error', function(e) {
+        if(!callbackDone) {
+            callbackDone = true;
+            callback("Error while request", e);
+        }
+    });
+
+    req.end();
+}
 
 function getDataFromRequest(req, callback) {
     var completeRequest = '';
